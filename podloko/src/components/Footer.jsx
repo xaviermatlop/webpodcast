5888d139712bccab9030d2b9ba382bd575423524
xaviermatlop <xaviermatlop@campus.monlau.com>
2026-04-13 16:44:25 +0200

    elimino carpeta podloko (proyecto duplicado)

--- podloko/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './Footer.css';
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2026 Morid Pecadores. Todos los derechos reservados.</p>
-    </footer>
-  )
-}
